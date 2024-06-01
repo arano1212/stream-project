3d5c27e06a462004cdb9a43fdab772b64460a319
arano1212 <arano1212@gmail.com>
2024-06-01 16:44:04 -0600

    check username

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { login, register, upload } from '../controllers/authController.js'
+import { checkUsarName, login, register, upload } from '../controllers/authController.js'
 
 const authRoutes = express.Router()
 
 authRoutes.post('/register', upload.single('avatar'), register)
 authRoutes.post('/login', login)
+authRoutes.get('/check-username/:username', checkUsarName)
 
 export default authRoutes
