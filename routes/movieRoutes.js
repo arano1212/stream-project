e0e56e57e88eedfedca05697f8688e7b03662333
arano1212 <arano1212@gmail.com>
2024-06-03 22:39:21 -0600

    creacion de nuevas peliculas

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { createMovie, deleteFilmById, getAllMovie, getMovieById, getMovieQuery, updateMovieById } from '../controllers/movieController.js'
+import { isAuth } from '../middlewares/isAuth.js'
 
 const movieRoutes = express.Router()
 
@@ -7,7 +8,7 @@ movieRoutes.post('/', createMovie)
 movieRoutes.get('/search', getMovieQuery)
 movieRoutes.get('/', getAllMovie)
 movieRoutes.get('/:movieId', getMovieById)
-movieRoutes.patch('/:movieId', updateMovieById)
+movieRoutes.patch('/:movieId', isAuth, updateMovieById)
 movieRoutes.delete('/:movieId', deleteFilmById)
 
 export default movieRoutes
